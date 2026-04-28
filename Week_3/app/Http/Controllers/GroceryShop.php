152e6afa1cb98a4abd52dc98b9d91da80c08c6da
pramodpandit72 <nwdpramod12305901@gmail.com>
2026-04-28 14:56:13 +0530

    Refactor getitems method in GroceryShop controller to clean up commented code

--- Week_3/app/Http/Controllers/GroceryShop.php
+++ Week_3/app/Http/Controllers/GroceryShop.php
@@ -9,8 +9,26 @@ class GroceryShop extends Controller
 {
     public function getitems(){
         // $items=\App\Models\Item::all();
-        $items=Item::select('name','price')->get();
-        return view('groceryshop', compact('items'));
+        // $items=Item::select('name','price')->get();
+        // $items=Item::where('price','>',50)->where('quantity','=',35)->get();
+        // $items=Item::orderBy('price','desc')->get();
+        // $items=Item::orderBy('price','asc')->get();
+        // $items=Item::orderBy('name','asc')->get();
+
+        // return view('groceryshop', compact('items'));
+
+        // $count=Item::count();
+        // return "Total Items in my db $count";
+
+        // $max=Item::max('price');
+        // return "maximum price of the items is $max";
+
+        // $items=Item::whereNot('price',50)->whereNot('name','tomato')->get();
+        // $items=item::wherePrice(50)->get();
+        // return $items;
+
+
+
         // return view('groceryshop',compact('items'));
         // $items = Item::all();
         // return view('items', compact('items'));
